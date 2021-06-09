caadd00a07b36ac711f0108a01467dab81db5167
mrprabhat010 <65821288+mrprabhat010@users.noreply.github.com>
2021-06-10 01:23:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/', route);
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 const port = process.env.PORT || 5000;
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
