da0747b8b6f3905f5bee0a69b40f87a4f17995b5
PRABHAT <mrprabhat010@gmail.com>
2021-06-10 12:13:06 +0530

    server update

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ require('dotenv').config({path: __dirname + '/.env'});
 require('./DBModule');
 const route = require('./Routes/routing');
 const app = express();
-
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use('/', route);
 app.use(express.static(path.join(__dirname, "client", "build")))
 
-const port = process.env.PORT || 5000;
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
